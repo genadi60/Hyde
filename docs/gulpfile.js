4dbeb95bcbcc92e0eb7aff9648819188d1d5e8f7
genadi60 <genadimihaylov@gmail.com>
2019-06-04 07:47:31 +0300

    Implement transition and animation

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -7,7 +7,7 @@ let gulp = require('gulp'),
 gulp.task('css', function () {
     return gulp.src('_assets/css/**/*.css')
         .pipe(autoprefixer())
-        .pipe(gulp.dest('docs/css/'))
+        .pipe(gulp.dest('./docs/css/'))
         .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
