72e4b541d67db6d27e7f0f10076180f9746f2df0
genadi60 <genadimihaylov@gmail.com>
2019-06-05 19:48:02 +0300

    Starting jekyll via gulp and added some excluded files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 'use strict';
 
 let gulp = require('gulp'),
-    autoprefixer = require('gulp-autoprefixer'),
-    browserSync = require('browser-sync').create();
+autoprefixer = require('gulp-autoprefixer'),
+browserSync = require('browser-sync').create(),
+exec = require('gulp-exec'),
+spawn = process.platform === 'win32' ? require('win-spawn') : require('child_process').spawn;
 
 gulp.task('css', function () {
     return gulp.src('_assets/css/**/*.css')
@@ -10,6 +12,19 @@ gulp.task('css', function () {
         .pipe(gulp.dest('./docs/css/'))
         .pipe(browserSync.stream({match: '**/*.css'}));
 });
+
+/*
+*  Jekyll*/
+
+gulp.task('jekyll', () =>  {
+    return spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: "inherit"});
+});
+
+/*gulp.task('build-jekyll', function () {
+    return gulp.src(['./!*.html', './_layouts/!**!/!*.html', './_posts/!**!/!*.markdown'])
+        .pipe(exec('jekyll build', options))
+        .pipe(exec.reporter(reportOptions));
+});*/
 
 gulp.task('watch', function () {
     browserSync.init({
@@ -20,9 +35,16 @@ gulp.task('watch', function () {
 
     gulp.watch('_assets/css/**/*.css', gulp.series('css'));
 
+    gulp.watch([
+        './*.html',
+        './_includes/*.html',
+        './_layouts/*.html',
+        './_posts/**/*.*'
+    ]).on('change', gulp.series('css', 'jekyll'));
+
     gulp.watch('docs/**/*.html').on('change', browserSync.reload);
     gulp.watch('docs/**/*.js').on('change', browserSync.reload);
 
 });
 
-gulp.task('default', gulp.series('css', 'watch'));
+gulp.task('default', gulp.series('css', 'jekyll', 'watch'));
