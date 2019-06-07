48a722a606e32250898c586e8049cb645d7ff2d8
genadi60 <genadimihaylov@gmail.com>
2019-06-07 21:20:09 +0300

    Implement products and changed some lines in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,23 @@ let gulp = require('gulp'),
 autoprefixer = require('gulp-autoprefixer'),
 browserSync = require('browser-sync').create(),
 exec = require('gulp-exec'),
-spawn = process.platform === 'win32' ? require('win-spawn') : require('child_process').spawn;
+cp = require('child_process');
+// spawn = process.platform === 'win32' ? require('win-spawn') : require('child_process').spawn;
+
 
 gulp.task('css', function () {
-    return gulp.src('./_assets/css/**/*.css')
+    return gulp.src('_assets/css/**/*.css')
         .pipe(autoprefixer())
         .pipe(gulp.dest('./docs/css/'))
         .pipe(browserSync.stream({match: '**/*.css'}));
 });
 
-/*
-*  Jekyll*/
+
+//  Jekyll
 
 gulp.task('jekyll', () =>  {
-    return spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: "inherit"});
+    return cp.spawn('bundle', ['exec', 'jekyll', 'build'], {stdio: "inherit", shell: true});
 });
-
-/*gulp.task('build-jekyll', function () {
-    return gulp.src(['./!*.html', './_layouts/!**!/!*.html', './_posts/!**!/!*.markdown'])
-        .pipe(exec('jekyll build', options))
-        .pipe(exec.reporter(reportOptions));
-});*/
 
 gulp.task('watch', function () {
     browserSync.init({
@@ -33,14 +29,14 @@ gulp.task('watch', function () {
         }
     });
 
-    gulp.watch(['_assets/css/**/*.css', 'css/**/*.css'], gulp.series('css'));
+    gulp.watch('_assets/css/**/*.css', gulp.series('css'));
 
     gulp.watch([
         './*.html',
         './_includes/*.html',
         './_layouts/*.html',
         './_posts/**/*.*'
-    ]).on('change', gulp.series('css', 'jekyll'));
+    ]).on('change', gulp.series('jekyll', 'css'));
 
     gulp.watch('docs/**/*.html').on('change', browserSync.reload);
     gulp.watch('docs/**/*.js').on('change', browserSync.reload);
